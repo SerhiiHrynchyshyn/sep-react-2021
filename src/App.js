0f9e485c96e62bbe6caec64fe5b90ce4c54acf71
SerhiiHrynchyshyn <server_059@ukr.net>
2022-01-15 18:27:50 +0200

    leson-1-cw

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <p>hello</p>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
 
 export default App;

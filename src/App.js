685ea1d18ba0bdcbaadb4f18ce4e18fc1a269192
SerhiiHrynchyshyn <server_059@ukr.net>
2022-01-15 18:42:17 +0200

    leson-2-hw

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, {useState} from "react";
+
 import './App.css';
+import Users from "./componens/Users/Users";
+import UserDetails from "./componens/UserDetails/UserDetails";
+import Posts from "./componens/Posts/Posts";
 
 function App() {
-  return (
-    <div className="App">
-      <p>hello</p>
-    </div>
-  );
+    const [user, setUser] = useState(null);
+    const [postId, setPostId] = useState(null);
+
+    const getUse = (user) => {
+        setUser(user);
+        setPostId(null)
+    }
+
+    const getPostId = (id) => {
+        setPostId(id)
+    }
+
+    return (
+        <div>
+            <div className={'wrap'}>
+                <Users getUse={getUse}/>
+                {user && <UserDetails user={user} getPostId={getPostId}/>}
+            </div>
+            {postId && <Posts postId={postId}/>}
+        </div>
+    );
 }
 
 export default App;

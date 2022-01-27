cc1b1b8b3f415c2f39652e5777b9247fc06e38e3
SerhiiHrynchyshyn <server_059@ukr.net>
2022-01-27 16:18:00 +0200

    finish fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 export * from './Layout/Layout';
 export * from './PostDetailsPage/PostDetailsPage';
+export * from './PostOfCommentsPage/PostOfCommentsPage';
 export * from './PostsPage/PostsPage';
 export * from './UserDetailsPage/UserDetailsPage';
 export * from './UsersOfPostsPage/UsersOfPostsPage';
